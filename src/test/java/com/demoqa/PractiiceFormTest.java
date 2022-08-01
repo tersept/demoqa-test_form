6656b2c45eacc8406744dd3f1151c1e864fe55df
Irina <pashaginy@rambler.ru>
2022-08-01 21:45:49 +0300

    added method fillForm and 3 field

---
+++ src/test/java/com/demoqa/PractiiceFormTest.java
@@ -0,0 +1,25 @@
+package com.demoqa;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class PractiiceFormTest {
+    @BeforeAll
+    static void configure(){
+        //Configuration.browserSize = '1300x952';
+    }
+
+    @Test
+    void fillForm(){
+    open("https://demoqa.com/automation-practice-form");
+    $("#firstName").setValue("Irina");
+    $("#lastName").setValue("Testova");
+    $("#userEmail").setValue("test@test.ru");
+    }
+}
